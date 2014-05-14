901bb6c002b695eb8167bd6807ec0fc4f59e6cdc
Dgo27 <diegoverona27@gmail.com>
2014-05-14 18:42:24 +0200

    -Migliorata UI lista mappe da completare

--- Treasure Hunt/src/tablet_unitn/treasurehunt/ContinueAdapter.java
+++ Treasure Hunt/src/tablet_unitn/treasurehunt/ContinueAdapter.java
@@ -23,12 +23,16 @@ public class ContinueAdapter extends ArrayAdapter<Map>{
         TextView name = (TextView)convertView.findViewById(R.id.continue_nomeMappa);
 		TextView distanza = (TextView)convertView.findViewById(R.id.continue_distanza);
 		TextView tempo_rimasto = (TextView)convertView.findViewById(R.id.continue_tempoRimasto);
+		TextView avanzamento_txt = (TextView)convertView.findViewById(R.id.continue_avanzamento_txt);
 		ProgressBar avanzamaneto = (ProgressBar)convertView.findViewById(R.id.continue_avanzamento);
         Map c = getItem(position);
         name.setText("Nome: "+c.getName());
-		distanza.setText("Distanza"+"13m");
-		tempo_rimasto.setText("Tempo rimanente"+"2h 13m");
-		avanzamaneto.setProgress(0);
+		distanza.setText("Distanza: "+"13m");
+		tempo_rimasto.setText("Tempo rimanente: "+"2h 13m");
+		Integer fatti = 75,
+				totale= 100;
+		avanzamento_txt.setText("Trovati "+fatti+" su "+totale);
+		avanzamaneto.setProgress(100*fatti/totale);
         return convertView;
     }
 }
