8ee96a0423d823deece3dee8c925d5bfa44a5479
Pietro <pietro@pietro-K53SV.(none)>
2014-05-22 12:24:52 +0200

    completato login

--- Treasure Hunt/src/tablet_unitn/treasurehunt/MainActivity.java
+++ Treasure Hunt/src/tablet_unitn/treasurehunt/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends FragmentActivity implements
 	private TabsPagerAdapter mAdapter;
 	private ActionBar actionBar;
 	// Tab titles
-	private String[] tabs = { "Home", "Continue", "New" };
+	private String[] tabs = { "Home", "New Game", "Continue" };
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
