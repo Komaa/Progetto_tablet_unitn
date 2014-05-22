4e79c388dde16d27426365c7da6ba18364812019
Pietro <pietro@pietro-K53SV.(none)>
2014-05-22 15:11:32 +0200

    completato login

---
+++ Treasure Hunt/src/tablet_unitn/dbmanager/Login_db.java
@@ -0,0 +1,53 @@
+package tablet_unitn.dbmanager;
+
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class Login_db extends AsyncTask<String, Integer, String>{
+
+	@Override
+	protected String doInBackground(String... params) {
+		// TODO Auto-generated method stub
+		String res=postData(params[0],params[1]);
+		
+		return res;
+	}
+
+	public String postData(String name, String psw) {
+		String result="";
+		HttpClient httpclient = new DefaultHttpClient();
+
+	    // Prepare a request object
+	    HttpGet httpget = new HttpGet("http://treasure-back.herokuapp.com/users/login/"+ name+"/"+psw); 
+
+	    // Execute the request
+	    HttpResponse response;
+	    try {
+	        response = httpclient.execute(httpget);
+	        result = EntityUtils.toString(response.getEntity());	 
+	        try {
+				JSONObject obj = new JSONObject(result);
+				result=obj.getString("success");
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	    }catch (ClientProtocolException e) {
+		// TODO Auto-generated catch block
+	} catch (IOException e) {
+			// TODO Auto-generated catch block
+	}
+	return result;  
+	}
+}
