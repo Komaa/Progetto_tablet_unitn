7451f4e47bd052b938ef8d06ef01bc2f73b59e5b
amk91 <agostino.pisanelli@gmail.com>
2014-05-28 15:45:54 +0200

    Immagini

--- Treasure Hunt/src/tablet_unitn/treasurehunt/ShowMapDetails.java
+++ Treasure Hunt/src/tablet_unitn/treasurehunt/ShowMapDetails.java
@@ -3,6 +3,7 @@ package tablet_unitn.treasurehunt;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,16 +17,24 @@ public class ShowMapDetails extends Activity {
         setContentView(R.layout.showmapdetails);
         
         TextView title = (TextView)findViewById(R.id.map_name);
+        TextView checkpoints = (TextView) findViewById(R.id.det_checkpoint);
+        TextView distance = (TextView) findViewById(R.id.det_distance);
+        TextView difficulty = (TextView) findViewById(R.id.det_difficulty);
+        TextView timeLeft = (TextView) findViewById(R.id.det_time);
+        TextView description = (TextView) findViewById(R.id.det_description);
+        
         Button b_showMap = (Button) findViewById(R.id.show_map);
+        
+        description.setMovementMethod(new ScrollingMovementMethod()); //rende scrollabile la descrizione
         
         String ID = (String) this.getIntent().getExtras().get(MainActivity.getAppContext()+".map_ID");
         
-        title.setText("ID mappa "+ID);
+        title.setText("ID mappa " + ID);
         
         b_showMap.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(getApplicationContext(),ShowMap.class);
+				Intent intent = new Intent(getApplicationContext(), ShowMap.class);
 			    /*String[] mio = new String[2];
 			    mio[0]= Integer.toString(123);
 			    intent.putExtra(MainActivity.getAppContext()+".item",mio);*/
