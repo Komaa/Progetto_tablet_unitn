5d48f0f59882c525b1a93080c42ced1d46813ca5
Dgo27 <diegoverona27@gmail.com>
2014-05-28 23:10:01 +0200

    Ripristinato stato sicuro?

--- Treasure Hunt/src/tablet_unitn/treasurehunt/NewAdapter.java
+++ Treasure Hunt/src/tablet_unitn/treasurehunt/NewAdapter.java
@@ -20,14 +20,26 @@ public class NewAdapter extends ArrayAdapter<Map>{
         LayoutInflater inflater = (LayoutInflater) getContext()
              .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         convertView = inflater.inflate(R.layout.newlist, null);
+        
         TextView name = (TextView)convertView.findViewById(R.id.new_nomeMappa);
+        TextView checkpoints = (TextView) convertView.findViewById(R.id.new_checkpoint);
 		TextView distanza = (TextView)convertView.findViewById(R.id.new_distanza);
 		TextView difficolta = (TextView)convertView.findViewById(R.id.new_difficolta);
 		
 		//Nome, descrizione, livello e num punti
 		Map c = getItem(position);
-        name.setText("Nome: "+c.getName());
-		distanza.setText("Distanza"+"boh");
+        name.setText(c.getName());
+        checkpoints.setText(c.getCount());
+        int level = c.getLevel();
+        if(level == 1){
+        	difficolta.setText("Easy");
+        } else if(level == 2){
+        	difficolta.setText("Medium");
+        } else if(level == 3){
+        	difficolta.setText("Hard");
+        }
+        
+        
         return convertView;
     }
 }
