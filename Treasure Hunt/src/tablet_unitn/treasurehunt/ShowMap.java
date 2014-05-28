f49e88ca24aff990f1f98a81f510fbdf201a7607
Dgo27 <diegoverona27@gmail.com>
2014-05-28 02:06:02 +0200

    -Aggiornato layout showmap -Aggiunta bussola (punta al Nord) -varie ed eventuali xD

--- Treasure Hunt/src/tablet_unitn/treasurehunt/ShowMap.java
+++ Treasure Hunt/src/tablet_unitn/treasurehunt/ShowMap.java
@@ -1,5 +1,8 @@
 package tablet_unitn.treasurehunt;
 
+import tablet_unitn.checkInternet.MobileInternetConnectionDetector;
+import tablet_unitn.checkInternet.WIFIInternetConnectionDetector;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -10,6 +13,10 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import android.app.Dialog;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
@@ -18,10 +25,14 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class ShowMap extends FragmentActivity implements LocationListener {
+public class ShowMap extends FragmentActivity implements LocationListener, SensorEventListener {
     GoogleMap googleMap;
     Location myLocation;
     TextView distance;
@@ -31,12 +42,46 @@ public class ShowMap extends FragmentActivity implements LocationListener {
     double dante_longitude = 11.120449;
     LatLng piazzaDante = new LatLng(dante_latitude, dante_longitude);
     
+    //Internet status flag
+    Boolean isMobileConnectionExist = false;
+    Boolean isWifiConnectionExist = false;
+    
+    // Connection detector class
+    MobileInternetConnectionDetector cd;
+    WIFIInternetConnectionDetector wc;
+    
+ // define the display assembly compass picture
+    private ImageView image;
+    // record the compass picture angle turned
+    private float currentDegree = 0f;
+    // device sensor manager
+    private SensorManager mSensorManager;
+    
+
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.showmap);
         distance = (TextView) findViewById(R.id.distance);
         Button piazzaDante = (Button) findViewById(R.id.b_piazzaDante);
+        
+        image = (ImageView) findViewById(R.id.imageViewCompass);
+        // initialize your android device sensor capabilities
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        
+        // creating connection detector class instance
+        cd = new MobileInternetConnectionDetector(getApplicationContext());
+        wc = new WIFIInternetConnectionDetector(getApplicationContext());
+        // get Internet status
+        isMobileConnectionExist = cd.checkMobileInternetConn();
+        isWifiConnectionExist = wc.checkMobileInternetConn();
+
+        // check for Internet status
+        if (!(isMobileConnectionExist||isWifiConnectionExist)) { //if not internet connection
+        	Toast.makeText(ShowMap.this, "No Internet Connection", Toast.LENGTH_LONG).show();
+        }
+        
         piazzaDante.setOnClickListener(new OnClickListener() {
 			
 			@Override
@@ -83,6 +128,19 @@ public class ShowMap extends FragmentActivity implements LocationListener {
             }
             locationManager.requestLocationUpdates(provider, 20000, 0, this);
             googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            
+         // Getting latitude of the current location
+            double latitude = myLocation.getLatitude();
+     
+            // Getting longitude of the current location
+            double longitude = myLocation.getLongitude();
+     
+            // Creating a LatLng object for the current location
+            LatLng latLng = new LatLng(latitude, longitude);
+     
+            //Sposta la camera in una posizione
+            CameraPosition myPosition = new CameraPosition.Builder().target(latLng).zoom(16).build();
+            googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(myPosition));
         }
     }
     @Override
@@ -97,6 +155,10 @@ public class ShowMap extends FragmentActivity implements LocationListener {
         // Creating a LatLng object for the current location
         LatLng latLng = new LatLng(latitude, longitude);
  
+        //Sposta la camera in una posizione
+        //CameraPosition myPosition = new CameraPosition.Builder().target(latLng).zoom(21).build();
+        //googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(myPosition));
+        
         // The computed distance is stored in results[0].
         //If results has length 2 or greater, the initial bearing is stored in results[1].
         //If results has length 3 or greater, the final bearing is stored in results[2].
@@ -104,13 +166,7 @@ public class ShowMap extends FragmentActivity implements LocationListener {
         Location.distanceBetween(latitude, longitude,
         		dante_latitude, dante_longitude, results);
         distance.setText("Distance: "+results[0]+" metres");
-        
-        //Sposta la camera in una posizione
-        CameraPosition myPosition = new CameraPosition.Builder().target(
-                latLng).zoom(21).build();
-
-        googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(myPosition));
- 
+         
     }
  
     @Override
@@ -141,4 +197,43 @@ public class ShowMap extends FragmentActivity implements LocationListener {
 
         googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(c_piazzaDante));
     }
+    
+    @Override
+    protected void onResume() {
+		super.onResume();
+		// for the system's orientation sensor registered listeners
+		mSensorManager.registerListener(this, mSensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION),
+		SensorManager.SENSOR_DELAY_GAME);
+    }
+    
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// to stop the listener and save battery
+		mSensorManager.unregisterListener(this);
+    }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // get the angle around the z-axis rotated
+        float degree = Math.round(event.values[0]);
+        // create a rotation animation (reverse turn degree degrees)
+        RotateAnimation ra = new RotateAnimation(
+            currentDegree,
+            -degree,
+	        Animation.RELATIVE_TO_SELF, 0.5f,
+	        Animation.RELATIVE_TO_SELF, 0.5f);
+        // how long the animation will take place
+    	ra.setDuration(210);
+    	// set the animation after the end of the reservation status
+    	ra.setFillAfter(true);
+    	// Start the animation
+    	image.startAnimation(ra);
+    	currentDegree = -degree;
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    	// not in use
+    }
+
 }
