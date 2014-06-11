016a353d4bc0c622ba3f76f35f3383e41a44d525
Pietro <pietro@pietro-K53SV.(none)>
2014-06-11 17:25:37 +0200

    corretti piccoli errori

--- Treasure Hunt/src/tablet_unitn/treasurehunt/ContinueFragment.java
+++ Treasure Hunt/src/tablet_unitn/treasurehunt/ContinueFragment.java
@@ -11,14 +11,18 @@ import tablet_unitn.treasurehunt.Map;
 import tablet_unitn.treasurehunt.ContinueAdapter;
 import tablet_unitn.treasurehunt.R;
 import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ContinueFragment extends Fragment {
